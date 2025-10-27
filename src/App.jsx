4c5521534dd4dc0b98b032ed9a6182075bba4116
Emmanuel Martin <em.martin456@gmail.com>
2025-10-27 09:16:32 +0100

    first commit: initialisation custom-hooks

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+const App = () => {
+
+
+  return (
+    <div className="container">
+      <h1 className="text-center">
+        Hello World
+      </h1>
+
+    </div>
+  )
+}
+
+export default App
